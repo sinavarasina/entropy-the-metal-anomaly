e284acecb824af08f1c999eda36f81fd285fce6c
sinavarasina <varasinafarmadani@gmail.com>
2025-11-28 02:21:27 +0700

    getting atleast bullet prototype working

--- entities/player/Cyborg/anim.gd
+++ entities/player/Cyborg/anim.gd
@@ -1,58 +1,25 @@
 extends Node
 class_name CyborgAnimation
 
-@onready var sprite: AnimatedSprite2D = get_parent().get_node("AnimatedSprite2D")
-#@onready var fireEf: AnimatedSprite2D = get_parent().get_node("FireEf")
+@onready var sprite: AnimatedSprite2D = get_parent().get_node("Pivot/AnimatedSprite2D")
+		
+func play_idle():
+	sprite.play("Idle2Laserbeam")
 
-var can_double_jump = false
+func play_run():
+	sprite.play("Run2Laserbeam")
 
-func set_can_double_jump(value: bool) -> void:
-	can_double_jump = value
+func play_jump():
+	sprite.play("Jump2Laserbeam")
 
-func update_animation(
-	dir: Vector2,
-	vel: Vector2,
-	body: CharacterBody2D,
-	jump_pressed: bool,
-	fire_pressed: bool,
-	delta: float
-) -> void:
-	var stats: CyborgStats = body.get_node("Stats")
-	var on_ground := body.is_on_floor()
+func play_double_jump():
+	sprite.play("DoubleJump")
 
-	# Flip sprite based on direction
-	if dir.x < 0:
-		sprite.flip_h = true
-	elif dir.x > 0:
-		sprite.flip_h = false
+func play_fall():
+	sprite.play("Fall")
 
-	# ====== LOGIKA FIRING ======
-	if fire_pressed:
-		sprite.play("FiringHotLaserbeam")
-		return
+func play_attack():
+	sprite.play("FiringHotLaserbeam")
 
-	# ====== LOGIKA ANIMASI ======
-	if on_ground:
-		# Gunakan run_threshold untuk menentukan Run vs Idle
-		if abs(dir.x) > stats.run_threshold:
-			sprite.play("Run2Laserbeam")
-		else:
-			sprite.play("Idle2Laserbeam")
-	else:
-		# Di udara
-		if vel.y < stats.jump_threshold:
-			# Lagi naik
-			if can_double_jump:
-				# Loncat pertama
-				sprite.play("Jump2Laserbeam")
-			else:
-				# Setelah double jump
-				sprite.play("DoubleJump")
-		else:
-			# Lagi jatuh (velocity.y > 0)
-			if dir.x == 0:
-				# Jatuh tanpa gerakan horizontal → Fall
-				sprite.play("Fall")
-			else:
-				# Jatuh dengan gerakan horizontal → Jump
-				sprite.play("Jump2Laserbeam")
+func is_finished() -> bool:
+	return not sprite.is_playing()
